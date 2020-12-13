3b3cfd0b528f640f7a45043bf0ca1d1ed720ef3b
MNITD <bogdan.kovalchuk@vgs.io>
2020-12-13 02:23:17 +0200

    [feat] add appendBuffer function

--- index.js
+++ index.js
@@ -152,6 +152,21 @@ const birthdayFunc = (isPlaying) => {
     })()
 }
 
+/**
+ * Creates a new Uint8Array based on two different ArrayBuffers
+ *
+ * @private
+ * @param {ArrayBuffers} buffer1 The first buffer.
+ * @param {ArrayBuffers} buffer2 The second buffer.
+ * @return {ArrayBuffers} The new ArrayBuffer created out of the two.
+ */
+const appendBuffer = function(buffer1, buffer2) {
+    const tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
+    tmp.set(new Uint8Array(buffer1), 0);
+    tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
+    return tmp.buffer;
+};
+
 (function () {
     'use strict';
     /**
